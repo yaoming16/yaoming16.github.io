229b8b712eb5cf92a5cfe7da32e96715a5b44992
Pablo Perez <pabloperezzz16@gmail.com>
2026-01-30 20:29:06 +0000

    Fixed links and added project

--- src/variables/projectsVar.jsx
+++ src/variables/projectsVar.jsx
@@ -10,6 +10,7 @@ import TOPHomePage from "../images/TOPHomepage.png";
 import BattleShip from "../images/BattleShip.png";
 import ToDo from "../images/ToDo.png";
 import CasaSerena from "../images/CasaSerena.png";
+import CVApplication from "../images/CVApplication.png";
 
 let projecstVar = {
   main: [
@@ -19,8 +20,8 @@ let projecstVar = {
       imgLink: MisCosas,
       tecnologies: ["JavaScript", "React", "TailwindCss"],
       btnLinks: [
-        "https://github.com/yaoming16/MisCosas-y-yo",
-        "https://yaoming16.github.io/MisCosas-y-yo/",
+        "https://github.com/yaoming16/Mis-cosas-y-yo/",
+        "https://yaoming16.github.io/Mis-cosas-y-yo/",
       ],
     },
     {
@@ -68,6 +69,16 @@ let projecstVar = {
       ],
     },
     {
+        title: "CV Application",
+        text: "A modern, accessible CV builder application built with React and Vite. Create, customize, and preview your professional CV with an intuitive interface.",
+        tecnologies: ["React", "Vite", "CSS"],
+        btnLinks: [
+          "https://github.com/yaoming16/TOP-CV-Application",
+          "https://top-cv-application-919.pages.dev/",
+        ],
+        imgLink: CVApplication,
+    },
+    {
       title: "Casa Serena",
       text: `Casa Serena is an elegant Spanish fine dining restaurant website that blends traditional Spanish culinary heritage with contemporary global influences. Located in the heart of Barcelona, this project demonstrates modern web development techniques.`,
       imgLink: CasaSerena,
@@ -111,7 +122,7 @@ let projecstVar = {
         "https://yaoming16.github.io/Product-Landing-Page--Project/",
       ],
     },
-    {
+    /*{
       title: "Technical Documentation Page",
       text: `This page contains technical documentation about javaScript. This page was made for a FreeCodeCamp project, that is why the information on the page
       comes directly from the example that was shown in the project requirements`,
@@ -121,7 +132,7 @@ let projecstVar = {
         "https://github.com/yaoming16/Technical-Documentation-Page",
         "https://yaoming16.github.io/Technical-Documentation-Page/",
       ],
-    },
+    },*/
     {
       title: "Random Quote Machine",
       text: `This simple app gives you a random quote every time that you click the 'New Quote' button. You can also tweet your quote if you wish. 
